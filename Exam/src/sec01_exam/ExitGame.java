a993629fa7275c3414bdd94ba11d04c49cf35511
Minkuy <81540740+Minkuy@users.noreply.github.com>
2021-04-07 09:42:39 +0900

    ExamLab1

---
+++ Exam/src/sec01_exam/ExitGame.java
@@ -0,0 +1,23 @@
+package sec01_exam;
+
+import java.util.Scanner;
+
+public class ExitGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+	int sum=0;
+	
+	for(int i=0; i<5; i++) {
+		System.out.println("입력");
+		int num = sc.nextInt();
+	sum+=num;
+	System.out.println("정수:"+num+",합:"+sum);
+	
+	if(sum>100) break;
+	}
+	System.out.println("프로그램 종료");
+	}
+
+}
---
+++ Exam/src/sec01_exam/SignPoint.java
@@ -0,0 +1,18 @@
+package sec01_exam;
+
+import java.util.Scanner;
+
+public class SignPoint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+
+System.out.println("정수입력:");
+int num = sc.nextInt();
+
+String result = num>0 ?"양수" : (num<0?"음수":"0");
+		System.out.println(result);
+	}
+
+}
